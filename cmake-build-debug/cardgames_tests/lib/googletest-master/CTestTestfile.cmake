8989d92302af4e34326d5992c1a58689249f605e
De'Toine Jones <djones26@mail.umw.edu>
2019-06-04 21:14:12 -0400

    Untracked cmake files

--- cmake-build-debug/cardgames_tests/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/djones/CLionProjects/CardGames/cardgames_tests/lib/googletest-master
-# Build directory: /Users/djones/CLionProjects/CardGames/cmake-build-debug/cardgames_tests/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/cardgames_tests/lib/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/djones/CLionProjects/CardGames/cardgames_tests/lib/googletest-master/googletest
-# Build directory: /Users/djones/CLionProjects/CardGames/cmake-build-debug/cardgames_tests/lib/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
