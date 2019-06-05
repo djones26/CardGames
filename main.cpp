23824d3ee2b33671ade4178facea47e7e63dc9e4
De'Toine Jones <djones26@mail.umw.edu>
2019-06-04 21:19:43 -0400

    Printing things

--- main.cpp
+++ main.cpp
@@ -12,7 +12,26 @@ void runTests();
 // MAIN
 
 int main( int argc, char** argv ) {
-    runTests();
+    //runTests();
+
+    Deck deck;
+
+    cout << deck << endl << endl << endl;
+
+    deck.shuffle();
+
+    cout << deck << endl;
+
+    Pile pile;
+
+    for ( int i = 0; i < 10; i++ ) {
+        Card* card = deck.drawCard();
+        pile.addCard(card);
+    }
+
+    cout << "Deck size: " << deck.numOfAvailableCards() << endl;
+    cout << "Pile size: " << pile.getSize() << endl;
+
 
     return 0;
 }
